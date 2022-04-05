3a8563a010977c0cb242ddbcc4e46b7ded6ea4d2
Lucas-far <mizoguchia@hotmail.com>
2022-04-05 20:06:29 -0300

    updates

--- js/teste.js
+++ js/teste.js
@@ -149,3 +149,95 @@ function getRndInteger(min, max, exact=false) {
 
 console.log(1200 / 1.4)
 console.log(1200 / 4)
+
+/*
+// JOGADOR
+let wolf = new Image()
+wolf.src = './assets/wolf_idle_right_tr.png'
+let wolfIdleLeft = new Image()
+wolfIdleLeft.src = './assets/wolf_idle_left_tr.png'
+let wolfWalkRight = new Image()
+wolfWalkRight.src = './assets/wolf_walk_right_tr.png'
+*/
+
+/*
+let wolfSetup = {
+  idleRight: {
+    widthList: [0, 184, 365, 542, 715, 892, 1074],
+    widthList2: [183, 183, 177, 171, 174, 180, 185],
+    srcHeight: 175,
+    frameAmount: 6
+  },
+  idleLeft: {
+    widthList: [0, 184, 365, 542, 715, 892, 1074],
+    widthList2: [183, 183, 177, 171, 174, 180, 185],
+    srcHeight: 175,
+    frameAmount: 6
+  },
+  walkRight: {
+    widthList: [0, 172, 350, 510, 685, 860, 1033, 1205.7, 1380],
+    widthList2: [171, 169, 160, 175, 175, 175, 175, 175, 175],
+    srcHeight: 161,
+    frameAmount: 8
+  }
+}
+*/
+
+/*
+// PARADO ->
+chars.push(
+  new Foe({
+    image: wolf,
+    widthList: [0, 184, 365, 542, 715, 892, 1074],
+    frameCounter: 0,
+    fixedHeight: 0,
+    widthList2: [183, 183, 177, 171, 174, 180, 185],
+    srcHeight: 175,
+    whereX: 100,
+    whereY: 400,
+    canvasWidth: 60,
+    canvasHeight: 60,
+    modularCounter: 0,
+    modular: 10,
+    frameAmount: 6
+  })
+)
+
+// CAMINHANDO ->
+chars.push(
+  new Foe({
+    image: wolfWalkRight,
+    widthList: [0, 172, 350, 510, 685, 860, 1033, 1205.7, 1380],
+    frameCounter: 0,
+    fixedHeight: 0,
+    widthList2: [171, 169, 160, 175, 175, 175, 175, 175, 175],
+    srcHeight: 161,
+    whereX: 100,
+    whereY: 400,
+    canvasWidth: 60,
+    canvasHeight: 60,
+    modularCounter: 0,
+    modular: 10,
+    frameAmount: 8
+  })
+)
+
+// PARADO <-
+chars.push(
+  new Foe({
+    image: wolfIdleLeft,
+    widthList: [0, 184, 365, 542, 715, 892, 1074],
+    frameCounter: 0,
+    fixedHeight: 0,
+    widthList2: [183, 183, 177, 171, 174, 180, 185],
+    srcHeight: 175,
+    whereX: 100,
+    whereY: 400,
+    canvasWidth: 60,
+    canvasHeight: 60,
+    modularCounter: 0,
+    modular: 10,
+    frameAmount: 6
+  })
+)
+*/
